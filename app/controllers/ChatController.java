2ebc6e7f2203b003c3f5a2a9ad0348eec753888b
qinxi <811153201@qq.com>
2016-05-06 14:46:59 +0800

    single rom

--- app/controllers/ChatController.java
+++ app/controllers/ChatController.java
@@ -8,6 +8,9 @@ import scala.concurrent.duration.Duration;
 import views.html.index;
 
 import javax.inject.Singleton;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.TimeUnit;
@@ -18,21 +21,39 @@ import java.util.concurrent.TimeUnit;
 @Singleton
 public class ChatController extends Controller {
 
+    private static Map<String,WebSocket.Out> outMap = new HashMap<>();
+
+    private static Map<String,String> users = new HashMap<>();
 
     public LegacyWebSocket<String> socket1(String name) {
+        String uuid = UUID.randomUUID().toString();
+
         System.out.println(name);
         return WebSocket.whenReady((in, out) -> {
+
+            outMap.forEach((id,out1)->{
+                out1.write(String.format("%s is online",name));
+            });
+
+
+            users.put(uuid,name);
+            outMap.put(uuid,out);
+
             // For each event received on the socket,
             in.onMessage((m) -> {
-                    System.out.println(m);
-                    out.write("Hello!");
+                outMap.forEach((id,out1)->{
+                    out1.write(String.format("%s:%s",name,m));
+                });
             });
 
             // When the socket is closed.
-            in.onClose(() -> System.out.println("Disconnected"));
-
-            // Send a single 'Hello!' message
-
+            in.onClose(() ->{
+                users.remove(uuid);
+                outMap.remove(uuid);
+                outMap.forEach((id,out1)->{
+                    out1.write(String.format("%s is logout",name));
+                });
+            });
         });
     }
 
